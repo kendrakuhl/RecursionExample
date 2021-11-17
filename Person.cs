13a7987f26d0ef95617b137ba54869e49547620e
Kendra Kuhl <kendra@kuhl.ws>
2021-11-17 16:58:18 -0600

    Initial commit

---
+++ Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RecursionExample
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<Person> Contacts { get; set; } = new List<Person>();
+        public string GetContacts()
+        {
+            StringBuilder retVal = new StringBuilder($"{FirstName} {LastName}'s contacts: {Environment.NewLine}");
+
+            foreach (Person person in Contacts)
+            {
+                if (person.Contacts.Any())
+                {
+                    retVal.Append(person.GetContacts());
+                }
+                else
+                {
+                    retVal.Append($"{person.FirstName} {person.LastName}{Environment.NewLine}");
+                }
+            }
+
+            return retVal.ToString();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecursionExample
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Person person = GetPersons();
+
+            Console.WriteLine(person.GetContacts());
+        }
+
+        public static Person GetPersons()
+        {
+            Person person = new Person
+            {
+                FirstName = "First",
+                LastName = "Level",
+                Contacts = new List<Person>
+                {
+                    new Person{ FirstName = "Second 1", LastName = "Level", Contacts = new List<Person>{ new Person{ FirstName = "Third 1.1", LastName = "Level" } } },
+                    new Person{
+                        FirstName = "Second 2",
+                        LastName = "Level",
+                        Contacts = new List<Person>(){
+                            new Person{ FirstName = "Third 2.1", LastName = "Level"},
+                            new Person { FirstName = "Third 2.2", LastName = "Level"}
+                        }}
+                }
+            };
+
+            return person;
+        }
+    }
+}
